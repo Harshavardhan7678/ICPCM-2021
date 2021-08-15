7e907710f3c6e054a03d68dd60b75e9ca63e5d6b
Harsha <81374435+Harshavardhan7678@users.noreply.github.com>
2021-08-16 00:52:47 +0300

    Changes over the Login page

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -12,66 +12,67 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import "./Registration.css";
-import BannerSmall from "../components/BannerSmall"
+import BannerSmall from "../components/BannerSmall";
+import "./Registration.css";
 const UserLogin = ({ handleChange }) => {
   const gridStyle = {
-    padding: 50,
+    padding: "18%",
   };
   const paperStyle = {
-    padding: 15,
-    height: "55vh",
-    width: 400,
-    margin: "0 auto",
+    padding: "3%",
+    height: "100%",
+    width: "100%",
+    margin: "auto",
     backgroundColor: "#E6E6FA",
   };
   const avatarStyle = { backgroundColor: "#1bbd7e" };
-  const btnstyle = { margin: "8px 0" };
+  const btnstyle = { margin: "auto" };
   const marginTop = { marginTop: "2%" };
   return (
     <>
-<BannerSmall/>
-    <Grid style={gridStyle} item xs zeroMinWidth>
-      <Paper style={paperStyle}>
-        <Grid align="center">
-          <Avatar style={avatarStyle}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <h2>Log In</h2>
-        </Grid>
-        <TextField
-          style={marginTop}
-          label="Username"
-          placeholder="Enter username"
-          fullWidth
-          required
-        />
-        <TextField
-          style={marginTop}
-          label="Password"
-          placeholder="Enter password"
-          type="password"
-          fullWidth
-          required
-        />
-        <FormControlLabel
-          style={marginTop}
-          control={<Checkbox name="checkedB" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          type="submit"
-          color="primary"
-          variant="contained"
-          style={btnstyle}
-          fullWidth
-        >
-          Log in
-        </Button>
-        <Typography style={marginTop}>
-          <Link href="#">Forgot password ?</Link>
-        </Typography>
-      </Paper>
-    </Grid>
+      <BannerSmall />
+      <Grid style={gridStyle} item xs zeroMinWidth>
+        <Paper style={paperStyle}>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2>Log In</h2>
+          </Grid>
+          <TextField
+            style={marginTop}
+            label="Username"
+            placeholder="Enter username"
+            fullWidth
+            required
+          />
+          <TextField
+            style={marginTop}
+            label="Password"
+            placeholder="Enter password"
+            type="password"
+            fullWidth
+            required
+          />
+          <FormControlLabel
+            style={marginTop}
+            control={<Checkbox name="checkedB" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            style={btnstyle}
+            fullWidth
+          >
+            Log in
+          </Button>
+          <Typography style={marginTop}>
+            <Link href="#">Forgot password ?</Link>
+          </Typography>
+        </Paper>
+      </Grid>
     </>
   );
 };
